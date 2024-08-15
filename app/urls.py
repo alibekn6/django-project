60367a2e75a219c254c459e3abd9f6668a2ae232
Alibek Anuarbek <alibekanuarbek@MacBook-Air--Alibek.local>
2024-08-15 17:10:16 +0500

    debugger

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from app.settings import DEBUG
 
 
 urlpatterns = [
@@ -7,3 +10,8 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog'))
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path('__debug__/', include("debug_toolbar.urls"))
+    ]
